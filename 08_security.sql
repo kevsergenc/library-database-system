2f8e33548b9605764cc2a23f67ae21fa48ee7a35
Kevser Genç <kevsergenc2003@gmail.com>
2026-05-31 23:02:32 +0300

    Update 08_security.sql

--- 08_security.sql
+++ 08_security.sql
@@ -1,28 +1,20 @@
-/* Role, Grant ve Revoke için */
-
 USE libraryDB;
 GO
 
--- 1. Roller Oluşturuluyor
-CREATE ROLE db_library_reader; -- Sadece okuma yapabilen rol (Örn: Öğrenciler/Ziyaretçiler)
-CREATE ROLE db_library_staff;  -- İşlem yapabilen rol (Örn: Kütüphane Görevlileri)
+CREATE ROLE db_library_reader; 
+CREATE ROLE db_library_staff; 
 
--- 2. Yetki Verme (GRANT)
--- Ziyaretçiler kitapları ve kategorileri görebilir
 GRANT SELECT ON Books TO db_library_reader;
 GRANT SELECT ON Categories TO db_library_reader;
 
--- Kütüphane görevlileri ana tabloları yönetebilir
 GRANT SELECT, INSERT, UPDATE, DELETE ON Books TO db_library_staff;
 GRANT SELECT, INSERT, UPDATE ON Borrowings TO db_library_staff;
 GRANT SELECT, INSERT, UPDATE ON Members TO db_library_staff;
 GRANT SELECT, INSERT, UPDATE ON Fines TO db_library_staff;
 
--- Kütüphane görevlilerinin oluşturulan prosedürleri çalıştırmasına izin ver
 GRANT EXECUTE ON sp_ReturnBook_Tx TO db_library_staff;
 GRANT EXECUTE ON sp_PayFine_Tx TO db_library_staff;
 
--- 3. Yetki Geri Alma/Kısıtlama (REVOKE)
--- Görevlilerin "Members" tablosundan üye silmesini kesinlikle engelle
+-- Görevlilerin "Members" tablosundan üye silmesini kesinlikle engeller
 REVOKE DELETE ON Members FROM db_library_staff;
 GO
